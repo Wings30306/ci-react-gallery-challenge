0acf97d5cc1aba992adc7edc546d8ea741862817
Joke Heyndels <jo_hannah@outlook.com>
2021-08-19 15:22:53 +0000

    Add css to functional component (challenge 2)

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import css from "./css/Sidebar.module.css"
 
 function Sidebar() {
     return (
-        <div>
-            <a></a>
-            <a></a>
-            <a></a>
+        <div className={css.sidebar}>
+            <a>My Photos</a>
+            <a>My Illustrations</a>
+            <a>My Paintings</a>
         </div>
     )
     

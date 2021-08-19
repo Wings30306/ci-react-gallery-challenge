52d1100bf7560facee9fa329983d206bb5d7162a
Joke Heyndels <jo_hannah@outlook.com>
2021-08-19 15:19:26 +0000

    Install dependencies, challenge 1

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div>
+            <a></a>
+            <a></a>
+            <a></a>
+        </div>
+    )
+    
+}
+
+export default Sidebar

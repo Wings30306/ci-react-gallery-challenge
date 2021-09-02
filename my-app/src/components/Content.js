413e6e3796087718ae12d2f7a44a3c33d773bb34
Joke Heyndels <jo_hannah@outlook.com>
2021-09-02 13:31:57 +0000

    Add loader to content to simulate API request

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react'
 import { savedPosts } from "../posts.json"
 import css from "./css/Content.module.css"
+import Loader from './Loader'
 import PostItem from './PostItem'
 
 export class Content extends Component {
     constructor(props) {
         super(props)
-    
+        console.log("Constructor triggered")
         this.state = {
-             
+            isLoaded: false,
         }
     }
-    
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+
     render() {
         return (
             <div className={css.Content}>
                 <h1 className={css.TitleBar}>My Posts</h1>
                 <div className={css.SearchResults}>
-                    {savedPosts.map(post => <PostItem className={css.SearchItem} key={post.name} post={post} />
+                    {this.state.isLoaded ? (
+                        savedPosts.map(post => <PostItem className={css.SearchItem} key={post.title} post={post} />
+                        )
+                    ) : (
+                        <Loader />
                     )}
                 </div>
             </div>
